72d0c77728a2245ae95f365551428d5ee96322c2
Shreyas Hegde <hegdeshreyas1408@gmail.com>
2026-07-21 15:03:04 +0530

    bfs and dfs adj list

---
+++ Templates/bfs.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+vector<vector<int>> adj;
+vector<bool> visited;
+
+void bfs(int start){
+    vector<bool> vis(adj.size() , false);
+    queue<int> q;
+    q.push(start);
+    vis[start] = true;
+
+    while(!q.empty()){
+        int u = q.front();
+        q.pop();
+        cout<< u <<" ";
+        for(int v : adj[u]){
+            if(!vis[v]){
+                vis[v] = true;
+                q.push(v);
+            }
+        }
+    }
+}
+
+
+int main(){
+    int n , m;
+    cin>>n>>m; // n nodes and m edges
+
+    adj.assign(n , {});
+    visited.assign( n ,false);
+    for(int i = 0; i < m; i++){
+        int  u , v; cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    bfs(0);
+    
+}
---
+++ Templates/dfs.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+vector<vector<int>> adj;
+vector<bool> visited;
+
+void dfs(int start){
+    visited[start] = true;
+    cout<<start<<" ";
+    for(int v : adj[start]){
+        if(!visited[v]){
+            dfs(v);
+        }
+    }
+
+}
+
+
+int main(){
+    int n , m;
+    cin>>n>>m; // n nodes and m edges
+
+    adj.assign(n , {});
+    visited.assign( n ,false);
+    for(int i = 0; i < m; i++){
+        int  u , v; cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    dfs(0);
+    
+}
