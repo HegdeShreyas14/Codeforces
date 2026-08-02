35c3b13c3c981475a98e47eb3228ae66d14fe331
Shreyas Hegde <hegdeshreyas1408@gmail.com>
2026-08-02 17:44:35 +0530

    post contest solves

--- B_Merge_to_Match.cpp
+++ B_Merge_to_Match.cpp
@@ -1,44 +1,59 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-int main(){
-    int tt;
-    cin>>tt;
-    
-    while(tt--){
-        int n, m;
-        cin>>n>>m;
-        
-        vector<int> a(n), b(m);
-        
-        for (int &x : a) cin >> x;
-        for (int &x : b) cin >> x;
-        
-        if (n < 2 * m) {
-            cout<<"NO"<<endl;
-            continue;
-        }
-        
-        sort(a.begin(), a.end());
-        sort(b.begin(), b.end());
+void solve() {
+    int n, m;
+    cin >> n >> m;
 
-        bool poss = true;
-        int i = 0;
-        
-        for (int j = 0; j < m; j++) {
-            while (i < n && a[i] < b[j]){
-                i++;
-            }
-            int cntL = i;
-            int l = j + 1;
-            int u = n - m + j;
-            if (cntL < l || cntL > u) {
-                poss = false;
-                break;
-            }
-        }
-        
-        cout<<(poss ? "YES" : "NO")<<endl;
+    vector<long long> a(n), b(m);
+    for (int i = 0; i < n; ++i) cin >> a[i];
+    for (int i = 0; i < m; ++i) cin >> b[i];
+
+    // 1. Since all elements are distinct, each element in 'b' requires
+    // merging at least 2 elements from 'a'. Thus n >= 2*m.
+    if (n < 2 * m) {
+        cout << "NO\n";
+        return;
     }
+
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+
+    // 2. Prefix / Suffix dominance check:
+    // For any threshold k, count(b > k) <= count(a > k) and count(b < k) <= count(a < k)
+    for (int i = 0; i < m; ++i) {
+        // count of elements in b <= b[i] is (i + 1)
+        // count of elements in a < b[i] must be at least (i + 1)
+        int count_a_less = lower_bound(a.begin(), a.end(), b[i]) - a.begin();
+        if (count_a_less < i + 1) {
+            cout << "NO\n";
+            return;
+        }
+
+        // count of elements in b >= b[i] is (m - i)
+        // count of elements in a > b[i] must be at least (m - i)
+        int count_a_greater = a.end() - upper_bound(a.begin(), a.end(), b[i]);
+        if (count_a_greater < m - i) {
+            cout << "NO\n";
+            return;
+        }
+    }
+
+    cout << "YES\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
     return 0;
 }
